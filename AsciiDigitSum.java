094d3f4d8e20fcedca8673d1d7c8938fa6427f99
Amarnath Sakthivel <63423850+Amarnath-Sakthivel@users.noreply.github.com>
2020-05-16 23:35:28 -0400

    Update AsciiDigitSum.java

--- AsciiDigitSum.java
+++ AsciiDigitSum.java
@@ -7,14 +7,16 @@ public class AsciiDigitSum {
          for (int i=0;i<input1.length;i++){                 // comparing input 1 with input 2.
              count=0;
              for (int j=0;j<input2.length;j++){
-                 if(input1[i]!=input2[j]){
-                     count++;
+                   if(Character.isUpperCase(input1[i]) && Character.isUpperCase(input2[j])){   //    // find ascii values for UpperCase
+                         if(input1[i]!=input2[j]){
+                         count++;
+                    }
                  }
              }if(count==input2.length){
 
-                 ascii=(int)Character.toUpperCase(input1[i]);     // find ascii values for UpperCase
+                 ascii=(int)(input1[i]); 
 
-                 System.out.println(Character.toUpperCase(input1[i])+" "+ascii);
+                 System.out.println((input1[i])+" "+ascii);     // find ascii values for UpperCase
                  ascii_sum+=ascii;                                // sum of ascii values for input 1
 
              }
@@ -22,13 +24,15 @@ public class AsciiDigitSum {
          for (int i=0;i<input2.length;i++){                     // comparing input 2 with input 1.
              count=0;
              for (int j=0;j<input1.length;j++){
-                 if(input2[i]!=input1[j]){
-                     count++;
+                   if(Character.isUpperCase(input1[i]) && Character.isUpperCase(input2[j])) {
+                         if(input2[i]!=input1[j]){
+                         count++;
+                    }
                  }
              }if(count==input1.length){
 
-                 ascii=(int)Character.toUpperCase(input2[i]);             // find ascii values for UpperCase
-                 System.out.println(Character.toUpperCase(input2[i])+" "+ascii);
+                 ascii=(int)(input2[i]);            
+                 System.out.println((input2[i])+" "+ascii);
                  ascii_sum+=ascii;                                          // sum of ascii values for input1(already found above) and input2
 
              }
